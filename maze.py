64bede6c32564641e11eb323ad5cce2d9235481c
luizhapsouza <luizhapsouza@gmail.com>
2024-06-20 19:50:02 -0300

    A* poor implemented

--- maze.py
+++ maze.py
@@ -1,5 +1,29 @@
 import os,time,sys,random
+
 start = time.time()
+
+def a_estrela_meia_boca(possibles):
+    h = 0
+    v = 1
+    print(end)
+    print(f'==================================\n{possibles}')
+    arr = []
+    for pos in possibles:
+        r = [0,0]
+        r[h] = end[h] - pos[h]
+        r[v] = end[v] - pos[v]
+
+        hip2 = r[h] ** 2 + r[v] ** 2
+        distance = hip2 ** (1/2)
+        arr.append(distance)
+
+    juntar_array = list(zip(arr, possibles))
+
+    array_ordenado = sorted(juntar_array, key=lambda x: x[0])
+
+    return [elem[1] for elem in array_ordenado]
+
+
 def get_rand_maze(size, chance):
     size += 2
     maze = []
@@ -18,9 +42,10 @@ def get_rand_maze(size, chance):
                 line.append(n)
         maze.append(line)
     init = (random.randint(1,size - 2), 0)
+    end = (random.randint(1,size - 2), size-1)
     maze[init[0]][init[1]] = 'A'
-    maze[random.randint(1,size - 2)][size-1] = 'B'
-    return [maze, init]
+    maze[end[0]][end[1]] = 'B'
+    return [maze, init, end]
 
 def display_maze(m, path):
     m2 = m[:]
@@ -46,8 +71,9 @@ def move(path):
     time.sleep(0.1)
     cur = path[-1]
     display_maze(maze,path)
-    possibles = [(cur[0],cur[1] + 1),(cur[0],cur[1]- 1),(cur[0] + 1, cur[1]),(cur[0]-1, cur[1]) ]
-    random.shuffle(possibles)
+    possibles2 = [(cur[0],cur[1] + 1),(cur[0],cur[1]- 1),(cur[0] + 1, cur[1]),(cur[0]-1, cur[1]) ]
+    possibles = a_estrela_meia_boca(possibles2)
+    # random.shuffle(possibles)
     
     
     for item in possibles:
@@ -62,7 +88,7 @@ def move(path):
             display_maze(maze,path)
             t = time.time() - start
             input(f"Solution found in {t} seconds! Press enter to finish")
-            os.system('cls') # windows use 'cls'
+            os.system('cls')
             sys.exit()
         else:
             newpath = path + (item,)
@@ -71,8 +97,7 @@ def move(path):
             display_maze(maze,path)
             time.sleep(0.1)
     
-
-[maze, init] = get_rand_maze(50, 25)
+[maze, init, end] = get_rand_maze(20, 20)
 
 move((init,))
 
